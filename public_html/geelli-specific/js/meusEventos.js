87fac46f8b60848f5e288961878bdfa79a9d9c76
GabrielaGSantos <gabi.gsantos@hotmail.com>
2017-07-05 21:43:54 -0400

    Fotos I SELP

--- public_html/geelli-specific/js/meusEventos.js
+++ public_html/geelli-specific/js/meusEventos.js
@@ -36,8 +36,7 @@ function botao(retorno) {
         success: function(data) {
             if (data.msg) {
                 $("#evento1").html('<p>Nenhum evento disponível</p><br>');
-                $(".eventosInscritor").html(' <div id="iscritoEvento1"><h3><a href="artigos/1a Circular - I SELP - IFMT.pdf">O Grupo de Estudos em Ensino de Línguas e Literatura - GEELLI e o Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso - Campus Cuiabá tem a satisfação de anunciar sucesso do I SELP - Simpósio sobre o Ensino de Língua Portuguesa, com o tema "Língua e Linguagens: mediação e construção de saberes para a docência na educação profissional. O evento foi realizado na sede do IFMT - Campus Cuiabá no período de 22 a 24 de junho de 2017, na Sala de Projeções do IFMT Campus Cuiabá Cel. Octayde Jorge da Silva. O GEELLI agradece a presença e participação de todos</p><ul class="actions"><li><a"class="button">Evento Concluído</a></li><li><a'
-                    '</a></li></ul></div>');
+                $(".eventosInscritor").html(' <div id="iscritoEvento1"><h3><a href="artigos/1a Circular - I SELP - IFMT.pdf">O Grupo de Estudos em Ensino de Línguas e Literatura - GEELLI e o Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso - Campus Cuiabá tem a satisfação de anunciar sucesso do I SELP - Simpósio sobre o Ensino de Língua Portuguesa, com o tema "Língua e Linguagens: mediação e construção de saberes para a docência na educação profissional. O evento foi realizado na sede do IFMT - Campus Cuiabá no período de 22 a 24 de junho de 2017, na Sala de Projeções do IFMT Campus Cuiabá Cel. Octayde Jorge da Silva. O GEELLI agradece a presença e participação de todos</p><ul class="actions"><li><a"class="button">Evento Concluído</a></li><li><a</a></li></ul></div>');
 
             }
         }
