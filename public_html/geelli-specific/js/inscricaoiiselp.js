ba1afff64eacc065bcb9afcbe2f21b8b7fe6072b
Vitor Bruno de Oliveira Barth <vbob@vbob.com.br>
2019-06-01 12:00:55 -0400

    Removida Aprsentação do II SELP

--- public_html/geelli-specific/js/inscricaoiiselp.js
+++ public_html/geelli-specific/js/inscricaoiiselp.js
@@ -7,61 +7,19 @@ function validateForm() {
         validation = false;
     }
 
-    var nomeTrabalho = document.forms["formularioInscricao"]["nomeTrabalho"].value;
-    if (tipoInscricao == '1' && validation && nomeTrabalho == '') {
-        alert('Digite o nome do trabalho a ser enviado');
-        validation = false;
-    }
-
-    var resumo = document.forms["arquivo"]["resumo"].value;
-    if (tipoInscricao == '1' && validation && resumo == '') {
-        alert('Selecione um arquivo contendo o Resumo');
-        validation = false;
-    }
-
     return validation;
-}
-
-function hideTrabalhos() {
-    $("input[name=nomeTrabalho]").hide();
-    $("label[for=nomeTrabalho]").hide();
-    $("input[name=resumo]").hide();
-    $("label[for=resumo]").hide();
-    $("input[name=nomeTrabalho]").val('');
-    $("input[name=resumo]").val('');
-    $(".dataApresentacao").hide()
-}
-
-function showTrabalhos() {
-    $("input[name=nomeTrabalho]").show();
-    $("label[for=nomeTrabalho]").show();
-    $("input[name=resumo]").show();
-    $("label[for=resumo]").show();
-    $(".dataApresentacao").show()
 }
 
 $(document).ready(function () {
     hideTrabalhos();
     $("select[name=tipoInscricao]").on('change', function () {
         var optionSelected = $("option:selected", this);
-        if (this.value == '1')
-            showTrabalhos();
-        else
-            hideTrabalhos();
     });
 
     $('#formularioInscricao').submit(function (e) {
         var url = "/eventos/iiselp/inscrever";
 
         e.preventDefault();
-        
-        $("#arquivo").submit(function (eventObj) {
-            $('<input />').attr('type', 'hidden')
-                .attr('name', "userId")
-                .attr('value', getUser().id)
-                .appendTo('#arquivo');
-            return true;
-        });
 
         if (validateForm()) {
             var jqXHR = jQuery.ajax({
@@ -69,17 +27,12 @@ $(document).ready(function () {
                 url: url,
                 async: true,
                 cache: false,
-                data: $("#formularioInscricao").serialize() + "&userId=" + getUser().id+ "&userName=" + getUser().nome,
+                data: $("#formularioInscricao").serialize() + "&userId=" + getUser().id + "&userName=" + getUser().nome,
                 dataType: 'json',
                 cache: false,
                 timeout: 5000,
                 success: function (data) {
-                    var tipoInscricao = document.forms["formularioInscricao"]["tipoInscricao"].value;
-                    if (tipoInscricao == '1') {
-                        uploadResumo();
-                    } else {
-                        window.location = '/usuarios/meusEventos';
-                    }
+                    window.location = '/usuarios/meusEventos';
                 },
                 error: function () {
                     alert('Erro ao concluir sua solicitação. Tente novamente.')
@@ -89,7 +42,3 @@ $(document).ready(function () {
         }
     });
 });
-
-function uploadResumo() {
-    $("#arquivo").submit();
-}
